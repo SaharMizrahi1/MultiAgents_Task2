882a6386f8ccc3e31a6fb4dadbcc9373012efe1d
Saharmizrahi1 <sahar3224@gmail.com>
2023-09-01 12:44:05 +0300

    Solution of yair

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,44 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+	public static void main(String[] args) throws InterruptedException {
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-            //test
-        }
-    }
+		// extract parameters
+		int n = Integer.valueOf(args[0]).intValue();
+		int d = Integer.valueOf(args[1]).intValue();
+		double p1 = Double.valueOf(args[2]).doubleValue();
+		double p2 = Double.valueOf(args[3]).doubleValue();
+
+		// generate and print CSP
+		Generator gen = new Generator(n, d, p1, p2);		
+		MASP masp = gen.generateMASP();
+		masp.print();
+
+		// initialize mailer
+		Mailer mailer = new Mailer();
+		for (int i = 0; i < n; i++) {
+			mailer.put(i);
+		}		
+
+		// create agents
+		ArrayList<Thread> threads = new ArrayList<Thread>();
+		for (int i = 0; i < n; i++) {
+			// use the csp to extract the private information of each agent
+			HashMap<Integer, ConsTable> private_information = masp.tablesOf(i);
+			Thread t = new Thread(new Agent(i, mailer, private_information, n, d));
+			threads.add(t);
+		}
+
+		// run agents as threads
+		for (Thread t : threads) {
+			t.start();
+		}
+
+		// wait for all agents to terminate
+		for (Thread t : threads) {
+			t.join();
+		}
+	}
 }
